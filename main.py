c7648bf7bffd87eb370ec4b8329b63ec6262c70e
VakhrushevGordey <Gordey777i@ya.ru>
2020-12-29 11:00:48 +0300

    наконец то я это сделал у меня добавлялся какой то левый файл и не добавлялся main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+
+from PyQt5 import uic
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+
+from random import randint
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Рисование')
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor("yellow"))
+        rad = randint(0, 150)
+        qp.drawEllipse(150 - rad, 150 - rad, 150 + rad, 150 + rad)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
